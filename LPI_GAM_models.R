cb1ae11363c55e5f7447414ff3f8b4c3075f2810
johngodlee <johngodlee@gmail.com>
2017-03-17 22:04:02 +0000

    Variable names

--- LPI_GAM_models.R
+++ LPI_GAM_models.R
@@ -18,25 +18,24 @@ load("LPIdata_Feb2016.RData")
 LPI <- LPIdata_Feb2016 %>%
   gather("year", "pop", 26:70)  %>%  # Transform to long format, gathering each 
   mutate(year = parse_number(.$year)) %>%  # Deprecated, extract_numeric() -> parse_numeric() -> parse_number(), extract numeric from atomic
-  mutate(., species = paste(Genus, Species, sep = ' ')) %>%  # Create a species column by concatenating genus and species
+  mutate(., genus_species = paste(Genus, Species, sep = '_')) %>%  # Create a species column by concatenating genus and species
   filter(., is.finite(pop)) %>%  # Keep only rows with a population estimate
   distinct(.) %>%  # Remove duplicate rows
-  group_by(., Common_Name, species, id) %>%  # group rows so that each group is one population (id) from one Species (Species+Common_Name)
+  group_by(., genus_species, id) %>%  # group rows so that each group is one population (id) from one Species(genus_species)
   mutate(., maxyear = max(year), minyear = min(year)) %>%  # Create a column for the max and min years for each group
   mutate(., lengthyear = maxyear-minyear) %>%  # Create a column for the length of time data available
-  filter(., lengthyear > 10) %>%  # Remove columns with less than 10 years of data
   ungroup(.) %>%  # Remove groupings
-  group_by(., Common_Name, species, id, Units) %>%  # Groups Measurement_type(Units)>population(id)>Species(Common_Name+Species)
+  group_by(., genus_species, id, Units) %>%  # Groups Measurement_type(Units)>population(id)>Species(genus_species)
   mutate(., scalepop = (pop-min(pop))/(max(pop)-min(pop))) %>%  # Scale population trend from 0 to 1
   filter(., is.finite(scalepop)) %>%  # Remove rows without a scalepop
   mutate(., meanpop = mean(pop)) %>%  # Create column for mean population
   ungroup(.) %>%
-  group_by(., Common_Name, species, id) %>%
+  group_by(., genus_species, id) %>%
   mutate(., meanpop.size = mean(meanpop)) %>%  # Create column for mean mean population
   ungroup(.)
 
 # Split the data frame into a list of data frames based on species, collection id, and units of collection ----
-LPI_list <- split(LPI, f = list(LPI$Species, LPI$id, LPI$Units))
+LPI_list <- split(LPI, f = list(LPI$genus_species, LPI$id, LPI$Units))
   # This crashes my laptop
   # Maybe book some time on the big computer
 
