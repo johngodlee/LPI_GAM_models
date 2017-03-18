1355c11936710492933c8d1c5cc80189e2521cc1
johngodlee <johngodlee@gmail.com>
2017-03-18 15:24:11 +0000

    John made list of GAM objects

--- LPI_GAM_models.R
+++ LPI_GAM_models.R
@@ -19,11 +19,12 @@ LPI <- LPIdata_Feb2016 %>%
   gather("year", "pop", 26:70)  %>%  # Transform to long format, gathering each 
   mutate(year = parse_number(.$year)) %>%  # Deprecated, extract_numeric() -> parse_numeric() -> parse_number(), extract numeric from atomic
   mutate(., genus_species = paste(Genus, Species, sep = '_')) %>%  # Create a species column by concatenating genus and species
-  filter(., is.finite(pop)) %>%  # Keep only rows with a population estimate
+  filter(., is.finite(pop)) %>% # Keep only rows with a population estimate
   distinct(.) %>%  # Remove duplicate rows
   group_by(., genus_species, id) %>%  # group rows so that each group is one population (id) from one Species(genus_species)
   mutate(., maxyear = max(year), minyear = min(year)) %>%  # Create a column for the max and min years for each group
   mutate(., lengthyear = maxyear-minyear) %>%  # Create a column for the length of time data available
+  filter(., lengthyear > 5) %>%  # Exclude columns where dataset <5 years 
   ungroup(.) %>%  # Remove groupings
   group_by(., genus_species, id, Units) %>%  # Groups Measurement_type(Units)>population(id)>Species(genus_species)
   mutate(., scalepop = (pop-min(pop))/(max(pop)-min(pop))) %>%  # Scale population trend from 0 to 1
@@ -38,8 +39,17 @@ LPI <- LPIdata_Feb2016 %>%
 LPI_list <- split(LPI, f = list(LPI$genus_species, LPI$id, LPI$Units))
   # This crashes my laptop
   # Maybe book some time on the big computer
+LPI_list <- split(LPI, interaction(LPI$genus_species), drop = TRUE)
+  # This works but will not make decent models as multiple population estimates
+LPI_list <- split(LPI, interaction(LPI$genus_species, LPI$id), drop = TRUE)
+  # This works and should not be any worse that including Units as well
+View(LPI_list[[1]])
 
-# Create GAM models for each list item using mgcv ----
+# Create GAMs and LMsfor each element in list ----
+gam_test <- gam(log10(pop)~year, data = LPI_list[[1]])
+summary(gam_test)
+
+lm_list <- lapply(LPI_list, function(x) lm(log10(pop)~year, data = x))
 
 # Average GAMs within each species ----
 
